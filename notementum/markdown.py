461b2d640f9f9effb3283bbcefb9b6c36b245db4
Ivan Fonseca <ivanfon@riseup.net>
2019-08-16 12:33:33 -0400

    Add preview HTML template

--- notementum/markdown.py
+++ notementum/markdown.py
@@ -6,6 +6,7 @@ from pygments import highlight
 from pygments.styles import get_style_by_name as get_style
 from pygments.lexers import get_lexer_by_name as get_lexer, guess_lexer
 from pygments.formatters.html import HtmlFormatter
+from pkg_resources import resource_filename
 
 
 # https://github.com/miyuchina/mistletoe/blob/master/contrib/pygments_renderer.py
@@ -15,17 +16,6 @@ from pygments.formatters.html import HtmlFormatter
 
 class NotementumRenderer(HTMLRenderer, LaTeXRenderer):
     # TODO: load from local file
-    mathjax_src = '''<script type="text/x-mathjax-config">
-        MathJax.Hub.Config({
-            tex2jax: {
-                inlineMath: [['$','$'], ['\\(','\\)']],
-                processEscapes: true,
-                scale: 125,
-                minScaleAdjust: 100,
-            },
-        });
-        </script>'''
-    mathjax_src += '<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"></script>'
 
     formatter = HtmlFormatter()
     formatter.noclasses = True
@@ -44,44 +34,18 @@ class NotementumRenderer(HTMLRenderer, LaTeXRenderer):
 
     def render_block_code(self, token):
         code = token.children[0].content
-        lexer = get_lexer(token.language) if token.language else guess_lexer(code)
+        if token.language:
+            lexer = get_lexer(token.language)
+        else:
+            lexer = guess_lexer(code)
         return highlight(code, lexer, self.formatter)
-
-    def render_document(self, token):
-        """
-        Append CDN link for MathJax to the end of <body>.
-        """
-        return super().render_document(token) + self.mathjax_src
 
 
 def gen_preview(content: str) -> str:
-        preview = '''<style>
-                       body {
-                         background-color: #404552;
-                         color: #d3dae3;
-                       }
-table {
-  border-collapse: collapse;
-}
-
-table, th, td {
-  border: solid black 1px;
-}
-
-th {
-  text-align: center;
-}
-
-th, td {
-  padding: 6px 10px;
-}
-
-tr:nth-child(even) {
-  background-color: #4b5160;
-}
-                     </style>'''
-
-        preview += mistletoe.markdown(content, NotementumRenderer)
-        print(preview)
+    with open(resource_filename('notementum', 'res/preview.html')) as f:
+        preview = f.read()
+        preview = preview.replace(
+            '{{{CONTENT}}}',
+            mistletoe.markdown(content, NotementumRenderer))
 
         return preview
