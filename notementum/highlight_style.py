2a90eac25b46908e37db0bc6c1a0836cb5e5dd7c
Ivan Fonseca <ivanfon@riseup.net>
2019-08-25 02:52:46 -0400

    Add license & copyright notice

--- notementum/highlight_style.py
+++ notementum/highlight_style.py
@@ -1,5 +1,23 @@
+# highlight_style.py - custom pygments syntax highlighting style
+# Copyright (C) 2019  Ivan Fonseca
+#
+# This file is part of Notementum.
+#
+# Notementum is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Notementum is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Notementum.  If not, see <https://www.gnu.org/licenses/>.
+
 from pygments.styles.monokai import MonokaiStyle
-from pygments.token import Name
+
 
 class NotementumStyle(MonokaiStyle):
     '''
--- notementum/views/main_window.py
+++ notementum/views/main_window.py
@@ -1,3 +1,21 @@
+# main_window.py - main window
+# Copyright (C) 2019  Ivan Fonseca
+#
+# This file is part of Notementum.
+#
+# Notementum is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Notementum is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Notementum.  If not, see <https://www.gnu.org/licenses/>.
+
 from typing import TYPE_CHECKING, Callable, Dict
 
 from gi.repository import Gdk, Gtk
